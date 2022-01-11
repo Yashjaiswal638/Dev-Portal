1b7e508c9bdc1e71defbf6f2f84cc733f924aaa2
Yashjaiswal638 <yjaiswal638@gmail.com>
2022-01-11 15:04:08 +0530

    setting templates folders & paths

--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    return HttpResponse('Following are the products')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse("<h2>Single Project</h2>")
+    return render(request, 'projects/single-project.html')
     
