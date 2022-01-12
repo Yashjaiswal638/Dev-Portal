b0a11bd14af47109bf4f3ad4b637e1b701e667d5
YashJ <yjaiswal638@gmail.com>
2022-01-12 14:56:03 +0530

    Added __str__ method to models.py-->class Project

--- projects/models.py
+++ projects/models.py
@@ -10,3 +10,6 @@ class Project(models.Model):
     sourceLink = models.CharField(max_length=1500, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
