5ec56989aebbb4af362ceaa372776c09edb706f0
YashJ <yjaiswal638@gmail.com>
2022-01-12 14:28:19 +0530

    database prep

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    demoLink = models.CharField(max_length=1500, null=True, blank=True)
+    sourceLink = models.CharField(max_length=1500, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
