8eecf15d349d83767025bf68c2d01b308ec74226
YashJ <yjaiswal638@gmail.com>
2022-01-14 10:00:26 +0530

    added update part of CRUD

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>', views.project, name="project"),
     path('createProject/', views.createProject, name="createProject"),
+    path('updateProject/<str:pk>', views.updateProject, name="updateProject"),
 
 ]
 
