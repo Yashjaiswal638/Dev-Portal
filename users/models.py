d6cc9c5a32f9ef0d7e796ba80c6d7fdd6fea6093
YashJ <yjaiswal638@gmail.com>
2022-01-26 12:39:27 +0530

    configured signals for post_save & post_delete

--- users/models.py
+++ users/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.signals import post_save, post_delete
+
+from django.dispatch import receiver
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -24,7 +28,7 @@ class Profile(models.Model):
 
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -38,3 +42,27 @@ class Skill(models.Model):
     
     def __str__(self):
         return str(self.name)
+
+# @receiver(post_save, sender = Profile)
+def createProfile(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            emails=user.email,
+            name=user.first_name,
+        )
+
+
+# @receiver(post_delete, sender = Profile)
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender=Profile)
+
+
+
